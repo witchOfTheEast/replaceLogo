6cc1507f3ef2027b3abb3c5dea6380385b4dead3
Randall Dunn <justThisGuyRandall@gmail.com>
2015-03-10 16:29:03 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# setup script for replaceLogo.py
+
+from setuptools import setup
+
+setup(
+    name='replaceLogo',
+    py_modules=['replaceLogo'],
+    install_requires=['BeautifulSoup4']
+)
