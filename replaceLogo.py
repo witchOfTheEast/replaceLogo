c46b9c5c1c4ef5b779441f31fbb1d9b596af1e51
Randall Dunn <justThisGuyRandall@gmail.com>
2015-03-20 11:18:02 -0700

    Script works on all MAXrm reports. Altered replace_attr_value to append any trailing string to attr_val_new in order to account for the relative file paths in Critical Event Reports.

--- replaceLogo.py
+++ replaceLogo.py
@@ -1,98 +1,188 @@
 #! /usr/bin/python
 
-# search through the provided .html for the search_string class
-# if found, replace the subsequent img src with the company logo
-# write out new .html file 
-
+# Takes an .html report file (MAXrm)
+# Replaces the logo and removes undesired links/images.
 # written for python 2.7
 
 from bs4 import BeautifulSoup as bsoup
 from sys import argv
-import os.path, re
+from os import path
+import re
 
-print "\n\n*************"
+script_loc = path.abspath(argv[0])
+script_dir = path.dirname(script_loc)
+company_logo_loc = path.abspath(path.join(script_dir, 'data/uiTechLogo.gif'))
 
-search_string = 'ReportLogo'
+def usage():
+    print """
+Usage: %s INPUTFILE [OUTPUTFILE] 
+    Modifies INPUTFILE, replacing logo and removing undesired elements.
+    Overwrites INPUTFILE or writes to OUTPUTFILE. Intended for use on html/xml.
+""" % path.basename(argv[0])
 
-if len(argv) > 3:
-    print 'Too many arguments provided on command line.'
-    exit(99)
-
-script_loc = os.path.abspath(argv[0])
-script_dir = os.path.dirname(script_loc)
-
-company_logo_loc = os.path.abspath(os.path.join(script_dir, 'data/uiTechLogo.gif'))
-
-try:
-    input_file_loc = argv[1]
+def replace_attr_value(soup_object, target_attr, attr_val_old, attr_val_new):
+    """Takes target_attribute, search_value, and replace_value and replaces matching attribute='search_value' with replace_value.
+    Matches ^ATTR_VAL_OLD(.*$), which assumes attr_val_old starts at the beginning of the string.
+    Captures everything else from the end of attr_val_old to the end of string, if anything, and concatenates the captured group to the end of attr_val_new."""
     
-    if os.path.isfile(input_file_loc) != True:
-        print '%r does not appear to be a valid file' % input_file_loc
-except IndexError:
-    print 'No files specified. Exiting.'
-    exit(99)
-
-try:
-    output_file_loc = os.path.join(script_dir, argv[2])
-except IndexError:
-    output_file_loc = input_file_loc
-    print 'No output file specified. Default output file is %s' % output_file_loc
-
-def abs_path(rel_path):
-    return os.path.abspath(rel_path)
-
-with open(input_file_loc) as f:
-    file_data = f.read()
-    f.close()
-
-soup_object = bsoup(file_data)
-
-org_encoding = soup_object.original_encoding
+    old_re_obj = re.compile(r'\A%s(.*$)' % attr_val_old, re.I)
+    old_attr_dict = {'%s' % target_attr: old_re_obj}
+    
+    new_re_obj = re.compile(attr_val_new, re.I)
+    new_attr_dict = {'%s' % target_attr: new_re_obj}
+    
+    new_tags = soup_object.find_all(attrs=new_attr_dict)
+    old_tags = soup_object.find_all(attrs=old_attr_dict)
+    
+    if len(new_tags) != 0:
+        print 'Already exists: '   
+        for item in new_tags:
+            print item
+     
+    if len(old_tags) == 0:
+        print 'Not found: ', attr_val_old
+         
+    else:
+        print 'Replacing: '
+        for item in old_tags:
+            old_match = old_re_obj.match(item[target_attr])
+            if old_match != None:
+                print 'Old: ', item
+                item[target_attr] = attr_val_new + old_match.group(1)
+                print 'New: ', item 
+    
+    # legacy code, did not capture groups after attr_val_old
+    """
+    if alt_tag != None:
+        print 'Already exists: ', alt_tag
+    
+    elif tag == None:
+        print 'Not found: ', attr_dict
    
-def replace_attr_value(target_attr, attr_val_old, attr_val_new):
-    """Takes target_attribute, search_value, and replace_value to find only first matching attribute='value' pair and replace with replace_value."""
-    attr_dict = {'%s' % target_attr: '%s' % attr_val_old}
-    print attr_dict
-    tag = soup_object.find(attrs=attr_dict)
-    tag[target_attr] = attr_val_new
-    print tag
-
-def strip_attr_line(target_attr, attr_val):
+    else:
+        tag[target_attr] = attr_val_new
+        print 'Old: ', attr_val_old 
+        print 'New: ', tag
+    """
+def strip_attr_line(soup_object, target_attr, attr_val):
     """Take target_attribute and target_value and destroy all matching (case-insensitive). Warning: Deletes matches regardless of tag. Also deletes match children."""
     re_obj = re.compile(attr_val, re.I)
     attr_dict = {'%s' % target_attr: re_obj}
     tags = soup_object.find_all(attrs=attr_dict)
-    print 'Removing: %d' % len(tags)
+    print 'Found: %d' % len(tags)
+    if len(tags) != 0:
+        print 'Removing: '
     for item in tags:
         print item
         item.decompose()
 
-def strip_tags(target_tag, target_attr=None):
+def strip_tags(soup_object, target_tag, target_attr=None):
     tags = soup_object.find_all(target_tag)
-    print 'Processing: %d' % len(tags)
-    for item in tags:
+    print 'I\'m looking for %s', target_tag
+    if len(tags) != 0:
+        remove_count = 0
+        left_count = 0
+        remove_list = []
+
+        for item in tags:
+            print 'start loop'
+            
+            if item.b:
+                print 'NOT removing: ', item
+                left_count += 1
+            else:
+                # no idea why append(item) puts '<NONE></NONE>' in the list
+                remove_list.append(repr(item))
+                item.decompose()
+                remove_count += 1
+        print 'Removed: %d' % remove_count
        
-        if item.b:
-            print 'NOT removing: ', item
-        else:
-            print 'Removing: ', item
-            print 'Removing contents:', item.contents
-            item.decompose()
+        for item in remove_list:
+            print item
+        
+        if remove_count + left_count != len(tags):
+            print 'WARNING: __main__.strip_tags\n remove_count + left_count != found tags.'
+            print 'The output file is may contain unexpected results.'
+            raw_input('Press any key to continue: ')
 
-print '# replace header logo'
-replace_attr_value('src', 'https://dashboard.systemmonitor.us/customisation/reseller/0/icon.gif', company_logo_loc)
+    else:
+        print 'Not found: ', target_tag
 
-print '# remove external link images'
-strip_attr_line('src', 'external-link')
+def writeFile(output_file, data):
+    with open(output_file, 'w') as f:
+        f.write(data)
+        f.close()
 
-print '# remove footer tag line'
-strip_attr_line('class', 'ReportFooter')
+def digest_soup(soup_object):
+    """Grouping of calls to process and modify file data."""
+    print 'Processing:'
+    
+    print '\n# replace header logo'
+    replace_attr_value(soup_object, 'src', 'https://dashboard.systemmonitor.us/customisation/reseller/0/icon.gif', company_logo_loc)
+    
+    print '\n# replace imcomplete image links'
+    replace_attr_value(soup_object, 'src', 'images/silk/', 'https://dashboard.systemmonitor.us/images/silk/')
+    
+    print '\n# remove external link images' # Patch reports
+    strip_attr_line(soup_object, 'src', 'external-link')
 
-print '# remove links'
-strip_tags('a')
+    print '\n# remove footer tag line'
+    strip_attr_line(soup_object, 'class', 'ReportFooter')
+
+    print '\n# remove links' # Critical Events reports
+    strip_tags(soup_object, 'a')
+
+    print ''
+
+def main(argv):
+    if len(argv) > 3:
+        print 'Too many arguments provided on command line.'
+        usage()
+        exit(99)
+
+    try:
+        input_file_loc = argv[1]
+        
+        if path.isfile(input_file_loc) != True:
+            print '%r does not appear to be a valid file' % input_file_loc
+            usage()
+            exit(99)
+    except IndexError:
+        print 'No files specified.'
+        usage()
+        exit(99)
+
+    try:
+        output_file_loc = path.join(script_dir, argv[2])
+    except IndexError:
+        output_file_loc = input_file_loc
+        print '\nNo output file specified on CLI. Using default.\nOutput file will be %s\n' % output_file_loc
+
+    def abs_path(rel_path):
+        return path.abspath(rel_path)
+
+    with open(input_file_loc) as f:
+        file_data = f.read()
+        f.close()
+
+    soup_object = bsoup(file_data)
+
+    org_encoding = soup_object.original_encoding
+     
+    digest_soup(soup_object)
+
+    edited_data = soup_object.prettify(org_encoding)
+
+    writeFile(output_file_loc, edited_data)
+
+if __name__ == '__main__':
+    main(argv)
 
 # legacy code before replace_attr_value with subtly different functionality
 """
+
+search_string = 'ReportLogo'
+
 desired_tag = soup_object.find('div', search_string)
 
 if desired_tag == None:
@@ -104,11 +194,4 @@ tag_to_edit = desired_tag.contents[0]
 tag_to_edit['src'] = abs_path(company_logo_loc)
 """
 
-edited_data = soup_object.prettify(org_encoding)
 
-def writeAllFiles():
-    with open(output_file_loc, 'w') as f:
-        f.write(edited_data)
-        f.close()
-
-writeAllFiles()
